cf8371ad40adb31944fb362995a1a93de8773b95
unknown <568385476@qq.com>
2023-01-04 23:29:39 +0800

    .....

---
+++ src/TextDocuments.ts
@@ -0,0 +1,64 @@
+import { DotLexer } from './dot/DotLexer';
+import { DotParser, Graph_listContext } from './dot/DotParser';
+import { CharStreams, CommonTokenStream } from 'antlr4ts';
+import { Uri, TextDocument } from 'vscode';
+
+type value = {
+  tokens: CommonTokenStream,
+  tree: Graph_listContext
+};
+
+
+class TextDocuments {
+
+  private documents: Map<Uri, value>;
+
+  constructor() {
+    this.documents = new Map();
+  }
+
+  public addDocument(document: TextDocument): void {
+    // 适用于第一次打开文档
+    if (this.documents.has(document.uri)) return;
+
+    this.updateDocument(document);
+  }
+
+  public removeDocument(document: TextDocument): void {
+    if (!this.documents.has(document.uri)) return;
+    this.documents.delete(document.uri);
+  }
+
+  public updateDocument(document: TextDocument): void {
+    const inputStream = CharStreams.fromString(document.getText());
+    const lexer = new DotLexer(inputStream);
+    const tokens = new CommonTokenStream(lexer);
+    const parser = new DotParser(tokens);
+    const tree = parser.graph_list();
+
+    this.documents.set(document.uri, { tokens, tree });
+  }
+
+  public getTokens(document: TextDocument): CommonTokenStream {
+    if(! this.documents.has(document.uri)) this.updateDocument(document);
+    const result = this.documents.get(document.uri) as value;
+    return result?.tokens;
+  }
+
+  public getTree(document: TextDocument): Graph_listContext {
+    if(! this.documents.has(document.uri)) this.updateDocument(document);
+    const result = this.documents.get(document.uri) as value;
+    return result.tree;
+  }
+
+  public contains(document: TextDocument) : boolean {
+    return this.documents.has(document.uri);
+  }
+
+}
+
+// 一个全局变量，用于缓存语法树和token流
+let textDocuments = new TextDocuments();
+export default textDocuments;
+
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,24 @@
 import * as vscode from 'vscode';
-import { DotLexer } from './dot/DotLexer';
-import { DotParser, Graph_listContext } from './dot/DotParser';
-import { CharStreams, CommonTokenStream, Token } from 'antlr4ts';
-import { assert } from 'console';
-
-
-// 只缓存 token 流和语法树。todo: 缓存报错信息
-export let documents: Map<vscode.Uri, {
-  tokens: CommonTokenStream,
-  tree: Graph_listContext
-}> = new Map();
+import textDocuments from './textDocuments';
+import { ExtensionContext } from 'vscode';
 
 
 export function activate(context: vscode.ExtensionContext) {
-  // 这条命令会输出在 调试控制台 中，如果没有显示终端，可以点击 终端 -> 显示正在运行的任务。
-  console.log('Congratulations, your extension "graphviz" is now active!');
+  // 事件绑定
+  bindEvents();
 
-  // 当关闭时会触发，打开时也会触发，关闭时 listener为 undefined, 注意打印 Uri 的时候需要调用toString
-  vscode.window.onDidChangeActiveTextEditor((editor) => {
-    if (editor == undefined || documents.has(editor.document.uri)) return;
-    parseDocument(editor.document);
-  });
-
-  vscode.workspace.onDidChangeTextDocument((e) => {
-    parseDocument(e.document);
-  });
+  // 注册 provider
 
 
-  const tokenTypes = ['namespace', 'keyword', 'class', 'enum', 'interface',
-    'struct', 'typeParameter', 'type', 'parameter', 'variable',
-    'property', 'enumMember', 'decorator', 'event', 'function',
-    'method', 'comment', 'string', 'number', 'regexp', 'function', 'macro',
+
+
+
+  const tokenTypes = [ 
+    // 'strict', 'graph', 'digraph', 'node', 'edge', 'subgraph', 'number', 
+    'method', 'function', 'function', 'type', 'type', 'function', 'number', 
+
+    //'string', 'id', 'html_string', 'comment', 'line_comment', 'PREPROC',
+    'string', 'keyword', 'regexp', 'comment', 'comment', 'comment',
   ];
 
   const legend = new vscode.SemanticTokensLegend(tokenTypes);
@@ -39,17 +27,12 @@ export function activate(context: vscode.ExtensionContext) {
     provideDocumentSemanticTokens(
       document: vscode.TextDocument
     ): vscode.ProviderResult<vscode.SemanticTokens> {
-      if (!documents.has(document.uri)) parseDocument(document);
-      assert(documents.has(document.uri));
-      let obj = documents.get(document.uri);
-      if (obj == undefined) return;
-      let { tokens } = obj;
+      const tokens = textDocuments.getTokens(document);
 
       const builder = new vscode.SemanticTokensBuilder(legend);
-      // const n = tokens.getNumberOfOnChannelTokens();
       for (const token of tokens.getTokens()) {
         if (token.type > 10)
-          builder.push(token.line - 1, token.charPositionInLine, token.text?.length || 0, 2);
+          builder.push(token.line - 1, token.charPositionInLine, token.text?.length || 0, token.type - 11);
       }
 
       return builder.build();
@@ -59,21 +42,16 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDocumentSemanticTokensProvider('dot', provider, legend);
 }
 
-// 生成语法树并缓存
-function parseDocument(document: vscode.TextDocument) {
-  const inputStream = CharStreams.fromString(document.getText());
-  const lexer = new DotLexer(inputStream);
-  const tokens = new CommonTokenStream(lexer);
-  const parser = new DotParser(tokens);
-  const tree = parser.graph_list();
 
-  documents.set(document.uri, { tokens, tree });
+// 绑定事件
+function bindEvents() {
+  // 当关闭时会触发，打开时也会触发，关闭时 listener为 undefined, 注意打印 Uri 的时候需要调用toString
+  vscode.window.onDidChangeActiveTextEditor((editor) => {
+    if (editor == undefined ) return;
+    textDocuments.addDocument(editor.document);
+  });
+
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    textDocuments.updateDocument(e.document);
+  });
 }
-
-/**
- * 当文件内容发生改变时触发
- * vscode.workspace.onDidChangeTextDocument 
- * 
- * 当打开文件发生变化时触发，注意，当切换文件时会触发两次
- */
-
