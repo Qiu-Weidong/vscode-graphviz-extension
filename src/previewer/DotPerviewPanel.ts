d5539e27f8aa00f355c66ae36b9222907343dee9
unknown <568385476@qq.com>
2023-01-13 12:00:18 +0800

    用于回退

---
+++ src/previewer/DotPerviewPanel.ts
@@ -0,0 +1,190 @@
+import { Disposable, Webview, WebviewPanel, window, Uri, ViewColumn } from "vscode";
+
+/**
+ * This class manages the state and behavior of HelloWorld webview panels.
+ *
+ * It contains all the data and methods for:
+ *
+ * - Creating and rendering HelloWorld webview panels
+ * - Properly cleaning up and disposing of webview resources when the panel is closed
+ * - Setting the HTML (and by proxy CSS/JavaScript) content of the webview panel
+ * - Setting message listeners so data can be passed between the webview and extension
+ */
+export class DotPreviewPanel {
+  public static currentPanel: DotPreviewPanel | undefined;
+  private readonly _panel: WebviewPanel;
+  private _disposables: Disposable[] = [];
+
+  /**
+   * The DotPreviewPanel class private constructor (called only from the render method).
+   *
+   * @param panel A reference to the webview panel
+   * @param extensionUri The URI of the directory containing the extension
+   */
+  private constructor(panel: WebviewPanel, extensionUri: Uri, title: string) {
+    this._panel = panel;
+
+    // Set an event listener to listen for when the panel is disposed (i.e. when the user closes
+    // the panel or when the panel is closed programmatically)
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // Set the HTML content for the webview panel
+    this._panel.webview.html = this._getWebviewWaitingContent(this._panel.webview, extensionUri, title);
+
+    // 设置图标
+    this._panel.iconPath = Uri.joinPath(extensionUri, 'media', 'icon.png');
+
+    // Set an event listener to listen for messages passed from the webview context
+    this._setWebviewMessageListener(this._panel.webview);
+  }
+
+  /**
+   * Renders the current webview panel if it exists otherwise a new webview panel
+   * will be created and displayed.
+   *
+   * @param extensionUri The URI of the directory containing the extension.
+   */
+  public static render(extensionUri: Uri, title: string, content: string) {
+    if (DotPreviewPanel.currentPanel) {
+      // 这里需要先判断渲染内容是否改变。
+
+
+      // If the webview panel already exists reveal it
+      DotPreviewPanel.currentPanel._panel.reveal(ViewColumn.One);
+    } else {
+      // If a webview panel does not already exist create and show a new one
+      const panel = window.createWebviewPanel(
+        // Panel view type
+        "preview",
+        // Panel title
+        title,
+        // The editor column the panel should be displayed in
+        ViewColumn.Beside,
+        // Extra panel configurations
+        {
+          // Enable JavaScript in the webview
+          enableScripts: true,
+        }
+      );
+
+      // 设置 currentPanel
+      DotPreviewPanel.currentPanel = new DotPreviewPanel(panel, extensionUri, title);
+    }
+  }
+
+  public static save(title: string, content: string) {
+
+  }
+
+  /**
+   * Cleans up and disposes of webview resources when the webview panel is closed.
+   */
+  public dispose() {
+    DotPreviewPanel.currentPanel = undefined;
+
+    // Dispose of the current webview panel
+    this._panel.dispose();
+
+    // 在这里进行一些善后处理
+
+    // Dispose of all disposables (i.e. commands) for the current webview panel
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+
+  /**
+   * Defines and returns the HTML that should be rendered within the webview panel.
+   *
+   * @remarks This is also the place where references to CSS and JavaScript files/packages
+   * (such as the Webview UI Toolkit) are created and inserted into the webview HTML.
+   *
+   * @param webview A reference to the extension webview
+   * @param extensionUri The URI of the directory containing the extension
+   * @returns A template string literal containing the HTML that should be
+   * rendered within the webview panel
+   */
+  private _getWebviewContent(webview: Webview, extensionUri: Uri) {
+    // 找到 toolkit.js ， 还要 asWebviewUri
+    const toolkitUri = webview.asWebviewUri(
+      Uri.joinPath(extensionUri, 'node_modules', '@vscode', 'webview-ui-toolkit', 'dist', 'toolkit.js')
+    );
+    // 找到 main.js
+    const mainUri = webview.asWebviewUri(
+      Uri.joinPath(extensionUri, 'media', 'main.js')
+    );
+
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+    return /*html*/`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <script type="module" src="${toolkitUri}"></script>
+          <script type="module" src="${mainUri}"></script>
+          <title>Hello World</title>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+          <vscode-button id="howdy">Howdy!</vscode-button>
+        </body>
+      </html>
+    `;
+  }
+
+  private _getWebviewWaitingContent(webview: Webview, extensionUri: Uri, title: string) {
+    // 找到 toolkit.js ， 还要 asWebviewUri
+    const toolkitUri = webview.asWebviewUri(
+      Uri.joinPath(extensionUri, 'node_modules', '@vscode', 'webview-ui-toolkit', 'dist', 'toolkit.js')
+    );
+
+    return /*html*/ `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <script type="module" src="${toolkitUri}"></script>
+          <title>${title}</title>
+        </head>
+        <body>
+          <h1>Please Wait</h1>
+          <vscode-button id="howdy">Howdy!</vscode-button>
+        </body>
+      </html>
+    `;
+  }
+
+  /**
+   * Sets up an event listener to listen for messages passed from the webview context and
+   * executes code based on the message that is recieved.
+   *
+   * @param webview A reference to the extension webview
+   * @param context A reference to the extension context
+   */
+  private _setWebviewMessageListener(webview: Webview) {
+    webview.onDidReceiveMessage(
+      (message: any) => {
+        const command = message.command;
+        const text = message.text;
+
+        switch (command) {
+          case "hello":
+            // Code that should run in response to the hello message command
+            window.showInformationMessage(text);
+            break;
+          // Add more switch case statements here as more webview message commands
+          // are created within the webview context (i.e. inside media/main.js)
+        }
+      },
+      undefined,
+      this._disposables
+    );
+  }
+}
